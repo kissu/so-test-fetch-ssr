7be124e967319d6bf596093d42139b1559806533
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-02-15 12:08:03 +0100

    fix: add more details on how it works

--- components/NestedContent.vue
+++ components/NestedContent.vue
@@ -3,7 +3,9 @@
     <p>Here is the NestedContent component and below is a slot passed to ParentWithSlots' component</p>
     <hr />
     <parent-with-slots>
+      <!-- <template #default> // this one can be omit since we do use the default slot here -->
       <p>This content is inserted into the component ParentWithSlots</p>
+      <!-- </template> -->
     </parent-with-slots>
   </div>
 </template>
--- components/ParentWithSlots.vue
+++ components/ParentWithSlots.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p>xxxxxxxxxxx ParentWithSlots' content before slot xxxxxxxxxxx</p>
-    <slot></slot>
+    <slot>Default content in case none is provided</slot>
     <p>xxxxxxxxxxx ParentWithSlots' content after slot xxxxxxxxxxx</p>
   </div>
 </template>
